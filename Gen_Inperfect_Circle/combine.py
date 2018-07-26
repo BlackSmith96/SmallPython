688c38cf584ad7e823faeb8931458f0b0d752400
blacksmith96 <blacksmith96@yahoo.com>
2018-07-26 17:01:33 -0400

    添加了execption检测

--- Gen_Inperfect_Circle/combine.py
+++ Gen_Inperfect_Circle/combine.py
@@ -6,10 +6,15 @@ def combine(img1, img2):
     img1 背景图片
     img2 要添加的图片，其除了物体之外为空白
     '''
+
+    if(img1.size != img2.size):
+        raise Exception("The size of two images are diffrent.")
+    
     width, height = img1.size
     default_color = (255,255,255)
     p1 = img1.load()
     p2 = img2.load()
+
     for i in range(width):
         for j in range(height):
             if(p2[i,j] != default_color):
