052842ead695784e24a443bf8c4152782c0a7576
blacksmith96 <blacksmith96@yahoo.com>
2018-07-26 16:47:46 -0400

    完成了两张图合成的函数

--- Gen_Inperfect_Circle/combine.py
+++ Gen_Inperfect_Circle/combine.py
@@ -6,3 +6,13 @@ def combine(img1, img2):
     img1 背景图片
     img2 要添加的图片，其除了物体之外为空白
     '''
+    width, height = img1.size
+    default_color = (255,255,255)
+    p1 = img1.load()
+    p2 = img2.load()
+    for i in range(width):
+        for j in range(height):
+            if(p2[i,j] != default_color):
+                 p1[i,j] = p2[i,j]
+
+    return img1
